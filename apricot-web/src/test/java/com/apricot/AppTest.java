f2b3a10bcd0ddffd2017297ae784146b17db0c89
Tarang <tp0265@gmail.com>
2020-04-30 02:12:19 +0530

    [UPDATE] added docker support

--- apricot-web/src/test/java/com/apricot/AppTest.java
+++ apricot-web/src/test/java/com/apricot/AppTest.java
@@ -4,17 +4,13 @@ import static org.junit.Assert.assertTrue;
 
 import com.apricot.api.UserApi;
 import com.apricot.configuration.ConfigurationTest;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 /**
  * Unit test for simple App.
