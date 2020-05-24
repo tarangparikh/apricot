c03e18e536a5eb44cc47f16da132a1ce48f76702
himani501 <himanibhardwaj501@gmail.com>
2020-05-24 08:18:41 +0530

    purchase-order-API

--- apricot-backend/src/main/java/com/apricot/core/business/repository/purchase/PurchaseOrderRepository.java
+++ apricot-backend/src/main/java/com/apricot/core/business/repository/purchase/PurchaseOrderRepository.java
@@ -1,14 +1,12 @@
 package com.apricot.core.business.repository.purchase;
-/* 
-    Apricot Management Suite
-    Copyright (C) 2020 Tarang Parikh
-    
-    Email : tp0265@gmail.com
-    Project Home : https://github.com/tarangparikh/apricot
-    
-    Original Author : @author Tarang Parikh <tp0265@gmail.com>
-    
-*/
 
-public class PurchaseOrderRepository {
+import com.apricot.core.model.category.Category;
+import com.apricot.core.model.purchase.PurchaseOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PurchaseOrderRepository extends JpaRepository {
+    List<PurchaseOrder> findAllByCompany_Id(Long id);
+    List<PurchaseOrder> findAllByParty_Id(Long id);
 }
