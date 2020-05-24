a0ef14cd4d020d47da46b62e77047910b02bab0a
himani501 <himanibhardwaj501@gmail.com>
2020-05-24 07:34:55 +0530

    CategoryApi

--- apricot-backend/src/main/java/com/apricot/core/business/repository/category/CategoryRepository.java
+++ apricot-backend/src/main/java/com/apricot/core/business/repository/category/CategoryRepository.java
@@ -11,7 +11,11 @@ package com.apricot.core.business.repository.category;
 */
 
 import com.apricot.core.model.category.Category;
+import com.apricot.core.model.units.Unit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category,Long> {
+    List<Category> findAllByCompany_Id(Long id);
 }
