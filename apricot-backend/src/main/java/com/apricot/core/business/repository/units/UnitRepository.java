3ef439adb9bbbbeba1a2ff2000b4da895e3caa79
himani501 <himanibhardwaj501@gmail.com>
2020-05-24 07:20:01 +0530

    Unit

--- apricot-backend/src/main/java/com/apricot/core/business/repository/units/UnitRepository.java
+++ apricot-backend/src/main/java/com/apricot/core/business/repository/units/UnitRepository.java
@@ -10,8 +10,12 @@ package com.apricot.core.business.repository.units;
     
 */
 
+import com.apricot.core.model.price.ServicePrice;
 import com.apricot.core.model.units.Unit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UnitRepository extends JpaRepository<Unit,Long> {
+    List<Unit> findAllByCompany_Id(Long id);
 }
