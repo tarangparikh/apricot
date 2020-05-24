78ffd1caa929265e73ed4f0cdfd4ad60e94f6061
Tarang <tp0265@gmail.com>
2020-05-24 08:46:48 +0530

    update

---
+++ apricot-backend/src/main/java/com/apricot/HomeController.java
@@ -0,0 +1,23 @@
+package com.apricot;
+/* 
+    Apricot Management Suite
+    Copyright (C) 2020 Tarang Parikh
+    
+    Email : tp0265@gmail.com
+    Project Home : https://github.com/tarangparikh/apricot
+    
+    Original Author : @author Tarang Parikh <tp0265@gmail.com>
+    
+*/
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = {"/", "/company/**", "/dashboard/**","/login/**"})
+    public String index() {
+        return "index.html";
+    }
+}
