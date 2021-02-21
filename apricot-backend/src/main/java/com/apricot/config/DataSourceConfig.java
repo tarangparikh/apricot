f756b697eff520f2e0dc60dc9b787d41f05939c6
tarang <tp0265@gmail.com>
2021-02-21 17:03:49 +0530

    [update]

--- apricot-backend/src/main/java/com/apricot/config/DataSourceConfig.java
+++ apricot-backend/src/main/java/com/apricot/config/DataSourceConfig.java
@@ -1,5 +1,6 @@
 package com.apricot.config;
 
+import org.graalvm.compiler.code.DataSection;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +9,23 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
+    private static final String databaseUrl = System.getenv("JDBC_DATABASE_URL");
+
 
     @Bean
     public DataSource getDataSource() {
+        if(databaseUrl!=null){
+            String dbUrl = System.getenv("JDBC_DATABASE_URL");
+            String username = System.getenv("JDBC_DATABASE_USERNAME");
+            String password = System.getenv("JDBC_DATABASE_PASSWORD");
+            DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+            dataSourceBuilder.driverClassName("org.postgresql.Driver");
+            dataSourceBuilder.url(dbUrl);
+            dataSourceBuilder.username(username);
+            dataSourceBuilder.password(password);
+            return dataSourceBuilder.build();
+        }
+
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.h2.Driver");
         dataSourceBuilder.url("jdbc:h2:mem:test");
